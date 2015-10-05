f4c8bbec853b66ada23a732b21e4e3ea3dc11d7f
Adam Kaplan <adamkaplan@yahoo-inc.com>
2015-10-05 10:00:23 -0700

    Add expiration date of development cert to logs

--- script/travis-codesign-setup.sh
+++ script/travis-codesign-setup.sh
@@ -12,9 +12,12 @@ security set-keychain-settings -t 3600 -l "$KEYCHAIN"
 
 # Download the certificate for the Apple Worldwide Developer Relations
 # Certificate Authority.
-CERTPATH="$SCRIPT_DIR/apple_wwdr.cer"
-curl 'https://developer.apple.com/certificationauthority/AppleWWDRCA.cer' > "$CERTPATH"
-security import "$CERTPATH" -k "$KEYCHAIN" -T /usr/bin/codesign
+CA_CERTPATH="$SCRIPT_DIR/apple_wwdr.cer"
+curl 'https://developer.apple.com/certificationauthority/AppleWWDRCA.cer' > "$CA_CERTPATH"
+security import "$CA_CERTPATH" -k "$KEYCHAIN" -T /usr/bin/codesign
 
 # Import our development certificate.
-security import "$SCRIPT_DIR/certificates/Development.p12" -k "$KEYCHAIN" -P "$KEY_PASSWORD" -T /usr/bin/codesign
+MY_CERTPATH="$SCRIPT_DIR/certificates/Development.p12"
+EXPIRY_DATE=`openssl pkcs12 -in "$MY_CERTPATH" -password "$KEY_PASSWORD" -nomacver -nokeys | openssl x509 -noout -enddate`
+echo "Codesign development certificate expiration: $EXPIRY_DATE"
+security import "$MY_CERTPATH" -k "$KEYCHAIN" -P "$KEY_PASSWORD" -T /usr/bin/codesign
